20da34b5e128ca339118868204cfbd5492320094
aishah849 <aishahalrashid@gmail.com>
2022-08-18 13:29:13 +0800

    Set up Search Engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} className="App-logo img-fluid" alt="logo" />
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by{" "}
+            <a
+              href="https://sg.linkedin.com/in/aishah-al-rashid-a4403015b"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Aishah Al-Rashid
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
